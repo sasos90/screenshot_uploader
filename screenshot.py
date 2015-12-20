bbf0b5d636514fdbf52b4d13e2024b17111148fb
Saso Sabotin <sasos90@gmail.com>
2015-12-20 22:19:34 +0100

    Upload file on dropbox and get url

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,5 @@
 import pyscreenshot as pyscreenshot
+import dropbox
 
 # part of the screen
 # bbox=(startX, startY, endX, endY))
@@ -8,4 +9,11 @@ import pyscreenshot as pyscreenshot
 # grab full printscreen and save it to file
 pyscreenshot.grab_to_file("printscreen.png")
 
-
+# authenticate dropbox with access token
+dbx = dropbox.Dropbox("access_token")
+# read the file
+file = open("printscreen.png", "rb").read()
+# upload the file
+dbx.files_upload(file, '/printscreenTIMESTAMP3.png')
+# get url of uploaded file
+fileUrl = dbx.sharing_create_shared_link("/printscreenTIMESTAMP3.png")
