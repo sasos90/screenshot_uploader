98e0b57d4bda56477ca37120963d99a9831f7819
Saso Sabotin <sasos90@gmail.com>
2015-12-23 19:24:41 +0100

    Loading values from config file

--- screenshot.py
+++ screenshot.py
@@ -5,23 +5,39 @@ import time
 import pyperclip
 import subprocess
 import os
+import os.path
+import ConfigParser
 
-# part of the screen
-# bbox=(startX, startY, endX, endY))
-# im = pyscreenshot.grab(bbox=(3000, 0, 4000, 1080))
-# im.save("printscreen.png", "PNG")
+# load config file
+Config = ConfigParser.ConfigParser()
+Config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__)),))
 
-# grab full printscreen and save it to file
-# pyscreenshot.grab_to_file("printscreen.png")
+# get values from config file
+_SCREENSHOT_PATH = Config.get("PathInfo", "ScreenshotPath")
+_SCREENSHOT_FILENAME = Config.get("PathInfo", "ScreenshotFilename")
+_ACCESS_TOKEN = Config.get("Dropbox", "AccessToken")
+
+fullFilePath = '/'.join([_SCREENSHOT_PATH, _SCREENSHOT_FILENAME])
 
 # grab selected screen with SCROT backend
-os.system("scrot -s printscreen.png");
+os.system("scrot -s %s" % (fullFilePath,));
 
 # authenticate dropbox with access token
-dbx = dropbox.Dropbox("4I36gE2ePRAAAAAAAAAAH_aIfXX0hvrFEA9HyuSGyrOHuUQo281LJz9rLiiqmsDm")
-# read the file
-openFile = open("printscreen.png", "rb")
-fileToUpload = openFile.read()
+dbx = dropbox.Dropbox(_ACCESS_TOKEN)
+
+fileExists = False
+counter = 0
+fileToUpload = False
+while fileExists is False and counter < 5:
+    if os.path.isfile(fullFilePath) is True:
+        # read the file
+        openFile = open(fullFilePath, "rb")
+        fileToUpload = openFile.read()
+        fileExists = True
+    else:
+        counter += 1
+        time.sleep(0.5)
+
 # upload the file
 uploadFileName = "/printscreen%s.png" % (time.time(),)
 dbx.files_upload(fileToUpload, uploadFileName)
@@ -33,7 +49,7 @@ fileUrl = metaData.url
 pyperclip.copy(fileUrl)
 
 # delete printscreenfile
-os.system("rm -rf printscreen.png")
+os.system("rm -rf %s" % (fullFilePath,))
 
 # show notification when file is uploaded properly
 subprocess.Popen(['notify-send', "Url copied to clipboard!\n\r%s" % (fileUrl,)])
